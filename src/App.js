5265498aca760a55e9a312a8c9bbc17e456342d3
Harsh Dhorajia <harshdhorajiya1304@gmail.com>
2020-08-13 17:35:30 +0530

    Adding the product component in the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import './App.css';
 import Header from './Components/Header';
+import Home from './Components/Home';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -8,16 +9,18 @@ function App() {
 
     <Router>
       <div className="App">
-        <Switch>
+        <Switch>  
           <Route path="/login">
             <h1>Login Page</h1>
           </Route>
           <Route path="/checkout">
+            <Header />
             <h1>checkout</h1>
           </Route>
           <Route path="/">
             {/*   <h1>Home page</h1> */}
             <Header />
+            <Home />
           </Route>
         </Switch>
       </div>
