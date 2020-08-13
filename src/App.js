afa6f5de219c7cb0ee02830beeed403dc55a3e3d
Harsh Dhorajia <harshdhorajiya1304@gmail.com>
2020-08-13 15:04:35 +0530

    Building Navbar using Router and styling with M-UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import Header from './Components/Header';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/login">
+            <h1>Login Page</h1>
+          </Route>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/">
+            {/*   <h1>Home page</h1> */}
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
