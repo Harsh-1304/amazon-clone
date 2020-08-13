9422ffddcc92e6d50f8c4b0663fd4c94ad52b3d4
Harsh Dhorajia <harshdhorajiya1304@gmail.com>
2020-08-13 19:06:50 +0530

    Adding reducer & StateProvider using context API

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,15 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { useStateValue } from './StateProvider';
 
 function Header() {
+    const [{ basket, user }] = useStateValue();
+    const login = () => {
+        if (user) {
+            auth.signOut();
+        }
+    };
     return (
         <nav className="header">
             {/* Logo */}
@@ -42,7 +49,7 @@ function Header() {
             <Link to="/checkout" className="header__link">
                 <div className="header__optionBasket">
                     <ShoppingBasketIcon />
-                    <span className="header__optionLineTwo header__basketCount">0</span>
+                    <span className="header__optionLineTwo header__basketCount">{basket.length}</span>
                 </div>
             </Link>
             </div>
